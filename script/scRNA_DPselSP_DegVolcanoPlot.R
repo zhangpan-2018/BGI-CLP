80195bdb253ffca6e7333305ea07555a885fe414
zhangpan-2018 <46232242+zhangpan-2018@users.noreply.github.com>
2025-02-18 09:31:02 +0800

    Update scRNA_DPselSP_DegVolcanoPlot.R

--- script/scRNA_DPselSP_DegVolcanoPlot.R
+++ script/scRNA_DPselSP_DegVolcanoPlot.R
@@ -1,4 +1,4 @@
-.libPaths("/data/public/zhangpanyu/Tools/R4.1.1/library")
+.libPaths("zhangpanyu/Tools/R4.1.1/library")
 
 args=commandArgs(T)
 if(length(args) != 4){ 
@@ -15,10 +15,10 @@ library(egg)
 library(ggrepel)
 library(Seurat)
 
-rdsfile=args[1]# /data/public/zhangpanyu/Project/CLP/scRNA/DPsel_DEGEnrich_2w-vs-0w_4w-vs-0w/0.data/CLP_0d7d10d14d28d.DPsel.rds
-dedir=args[2] # /data/public/zhangpanyu/Project/CLP/scRNA/DPsel_DEGEnrich_2w-vs-0w_4w-vs-0w/DegVolcano/DEG
-group=args[3] # CLP_4w-vs-CLP_0w
-outdir=args[4] # /data/public/zhangpanyu/Project/CLP/scRNA/DPsel_DEGEnrich_2w-vs-0w_4w-vs-0w/DegVolcano/VolcanoPlot
+rdsfile=args[1]# 
+dedir=args[2] # 
+group=args[3] # 
+outdir=args[4] #
 if(!dir.exists(outdir)){dir.create(outdir,recursive=T)}
 casename=strsplit(group,"-vs-")[[1]][1]
 contralname=strsplit(group,"-vs-")[[1]][2]
@@ -66,42 +66,3 @@ for (i in celltype){
 }
 
 
-#sel2w=sel[,sel$time=="CLP_14d"]
-#sel12w=sel2w[,Idents(sel2w)=="DPsel1"]
-#sel0w=sel[,sel$time=="CLP_0w"]
-#sel10w=sel0w[,Idents(sel0w)=="DPsel1"]
-#sel1deg=read.csv("/data/public/zhangpanyu/Project/CLP/scRNA/DPsel_DEGEnrich_2w-vs-0w_4w-vs-0w/DEG/CLP_10d-vs-CLP_0w/DEG_CLP_10d-vs-CLP_0w_DPsel1.CSV")
-#
-#aveexp0w=AverageExpression(sel0w)
-#aveexp0w=as.data.frame(aveexp0w$RNA)
-#aveexp2w=AverageExpression(sel2w)
-#aveexp2w=as.data.frame(aveexp2w$RNA)
-#
-#exp0w=aveexp0w[sel1deg$X,]
-#exp0w=exp0w$DPsel1
-#exp2w=aveexp2w[sel1deg$X,]
-#exp2w=exp2w$DPsel1
-#
-#### 
-#plotfile=cbind(sel1deg,exp0w,exp2w)
-#
-#plotfile$exp0wlog=log2(plotfile$exp0w+1)
-#plotfile$exp2wlog=log2(plotfile$exp2w+1)
-#
-##sp_scatterplot(plotfile, xvariable = "exp2w", yvariable = "exp0w", 
-##                 color_variable = "updown",
-##                title ="DPsel1 2w-vs-0w", 
-##               color_variable_order = c("up", "down"),
-##              manual_color_vector = c("firebrick","dodgerblue")) + 
-##  coord_fixed(1)+ labs(x = "exp2w", y = "exp0w")
-#
-#pdf("/data/public/zhangpanyu/Project/CLP/scRNA/DPsel_DEGEnrich_2w-vs-0w_4w-vs-0w/test_voc.pdf")
-#sp_scatterplot(plotfile, xvariable = "exp2wlog", yvariable = "exp0wlog", 
-#                 color_variable = "updown",
-#                title ="DPsel1 2w-vs-0w", 
-#               color_variable_order = c("up", "down"),
-#              manual_color_vector = c("firebrick","dodgerblue")) + 
-#  coord_fixed(1)+ labs(x = "exp2wlog", y = "exp0wlog")
-#dev.off()
-#
-#
